07f31e96299c979896101b935fde855913efcb49
imdanix <idanix@list.ru>
2019-04-10 00:47:24 +0300

    Do not search for foodItem if eater is found

--- src/main/java/me/imdanix/food/PlayerListener.java
+++ src/main/java/me/imdanix/food/PlayerListener.java
@@ -20,55 +20,60 @@ public class PlayerListener implements Listener {
 
 	@EventHandler
 	public void onInteract(PlayerInteractEvent e) {
-		if(!e.getPlayer().hasPermission("xfood.eat"))
+		Player p=e.getPlayer();
+		if(!p.hasPermission("xfood.eat"))
 			return;
 		Action act=e.getAction();
 		if(act==Action.PHYSICAL)
 			return;
 		if(act==Action.LEFT_CLICK_AIR||act==Action.LEFT_CLICK_BLOCK)
-			removeEater(e.getPlayer().getUniqueId());
+			removeEater(p.getUniqueId());
 		else {
-			Player p=e.getPlayer();
 			if(act==Action.RIGHT_CLICK_BLOCK) {
 				if(p.isSneaking())
 					return;
 				e.setCancelled(true);
 			}
 			Eater eater=getEater(p.getUniqueId());
-			EquipmentSlot hand=EquipmentSlot.HAND;
-			FoodItem foodItem=getFood(p.getInventory().getItemInMainHand());
-			if(foodItem==null) {
-				foodItem=getFood(p.getInventory().getItemInOffHand());
-				if(foodItem==null)
-					return;
-				hand=EquipmentSlot.OFF_HAND;
-			}
 			if(eater!=null) {
 				sendDebug(p, "Eater found");
-				if(foodItem.getId().equals(eater.getFood())) {
-					double time=(System.currentTimeMillis()-eater.getStartTime());
-					if(time<getMaxTime()) {
-						sendDebug(p, "Eating");
-						p.addPotionEffect(SLOW_EFFECT);
-						World w = p.getWorld();
-						w.playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_EAT, 1 , 1);
-						w.spawnParticle(Particle.ITEM_CRACK,p.getEyeLocation(), 6,  0.1,0.1,0.1, 0.01, foodItem);
-						eater.addClick();
-						if(time>getMinTime()&&eater.getClicks()>=getMinClicks()) {
-							sendDebug(p, "Eating finished");
-							PlayerInventory inv=p.getInventory();
-							if(hand==EquipmentSlot.HAND)
-								inv.setItemInMainHand(Utils.removeItem(inv.getItemInMainHand()));
-							else
-								inv.setItemInOffHand(Utils.removeItem(inv.getItemInOffHand()));
-							foodItem.eat(p);
-							p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_PLAYER_BURP, 1 , 1);
-						} else return;
+				FoodItem foodItem=getFood(eater.getFood());
+				PlayerInventory inv=p.getInventory();
+				EquipmentSlot hand=EquipmentSlot.HAND;
+				if(!foodItem.isSimilar(inv.getItemInMainHand())) {
+					if(!foodItem.isSimilar(inv.getItemInOffHand())) {
+						sendDebug(p, "Removing eater");
+						removeEater(e.getPlayer().getUniqueId());
+						return;
 					}
+					hand=EquipmentSlot.OFF_HAND;
+				}
+				double time=(System.currentTimeMillis()-eater.getStartTime());
+				if(time<getMaxTime()) {
+					sendDebug(p, "Eating");
+					p.addPotionEffect(SLOW_EFFECT);
+					World w = p.getWorld();
+					w.playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_EAT, 1 , 1);
+					w.spawnParticle(Particle.ITEM_CRACK,p.getEyeLocation(), 10,  0.1,0.1,0.1, 0.001, foodItem);
+					eater.addClick();
+					if(time>getMinTime()&&eater.getClicks()>=getMinClicks()) {
+						sendDebug(p, "Eating finished");
+						if(hand==EquipmentSlot.HAND)
+							inv.setItemInMainHand(Utils.removeItem(inv.getItemInMainHand()));
+						else
+							inv.setItemInOffHand(Utils.removeItem(inv.getItemInOffHand()));
+						foodItem.eat(p);
+						p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_PLAYER_BURP, 1 , 1);
+					} else return;
 				}
 				sendDebug(p, "Removing eater");
 				removeEater(e.getPlayer().getUniqueId());
 			} else {
+				FoodItem foodItem=getFood(p.getInventory().getItemInMainHand());
+				if(foodItem==null)
+					foodItem=getFood(p.getInventory().getItemInOffHand());
+				if(foodItem==null)
+					return;
 				sendDebug(p, "New eater");
 				addEater(p.getUniqueId(), foodItem.getId());
 			}
